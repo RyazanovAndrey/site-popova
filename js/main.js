55dfce08a4fdfde6449c6f71f88185801d69683a
RyazanovAndrey <ryazanov.andrej@gmail.com>
2026-01-06 12:52:30 +0200

    end portfolio

--- js/main.js
+++ js/main.js
@@ -4,3 +4,31 @@ const typed = new Typed('#element', {
     backSpeed: 100,
     loop: true
 });
+
+// Latest Portfolio
+const portfolioTabs = document.querySelector('.portfolio-tabs')
+const tabs = document.querySelectorAll('.portfolio-tab')
+const cardTab = document.querySelectorAll('.card-tab')
+
+portfolioTabs.addEventListener('click', (event) => {
+
+    const currentClick = event.target
+
+    tabs.forEach(item => item.classList.remove('active'))
+    currentClick.classList.add('active')
+    
+    const tabData = currentClick.dataset.tab
+    
+    cardTab.forEach(tab => {
+
+        tab.classList.remove('hidden')
+
+        if(tabData == 'all') return true
+
+        if(tab.dataset.tab !== tabData) {
+            tab.classList.add('hidden')
+        }
+    })
+})
+
+// --------------------
