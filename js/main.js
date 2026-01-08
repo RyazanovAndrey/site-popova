6505193b4e51ef84ac584033e9331e50f77c6554
RyazanovAndrey <ryazanov.andrej@gmail.com>
2026-01-08 10:28:31 +0200

    end price

--- js/main.js
+++ js/main.js
@@ -16,19 +16,34 @@ portfolioTabs.addEventListener('click', (event) => {
 
     tabs.forEach(item => item.classList.remove('active'))
     currentClick.classList.add('active')
-    
+
     const tabData = currentClick.dataset.tab
-    
+
     cardTab.forEach(tab => {
 
         tab.classList.remove('hidden')
 
-        if(tabData == 'all') return true
+        if (tabData == 'all') return true
 
-        if(tab.dataset.tab !== tabData) {
+        if (tab.dataset.tab !== tabData) {
             tab.classList.add('hidden')
         }
     })
 })
 
-// --------------------
+// Price plan
+
+const priceTabs = document.querySelector('.price-tabs')
+const priceTab = document.querySelectorAll('.price-tab ') 
+
+priceTabs.addEventListener('click', (event) => {
+    const currentClick = event.target
+
+    priceTab.forEach(item => item.classList.remove('active'))
+    currentClick.classList.add('active')
+
+    const dataTab = event.target.dataset.tab
+    
+    document.querySelectorAll('.price-monthly').forEach(item => item.classList.add('hidden'))
+    document.querySelector(`#${dataTab}`).classList.remove('hidden')
+})
